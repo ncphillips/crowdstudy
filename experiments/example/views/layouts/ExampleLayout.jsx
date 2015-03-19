c480d0f3c430bffc174ebedd72fecb5526494c39
ncphillips <ncphillips@upei.ca>
2015-03-19 10:36:37 -0300

    arg

--- experiments/example/views/layouts/ExampleLayout.jsx
+++ experiments/example/views/layouts/ExampleLayout.jsx
@@ -9,23 +9,18 @@ var React = require('react');
 var ExampleLayout = React.createClass({
     render: function () {
         return (
-            <html>
-                <head>
-                    <title>{this.props.title}</title>
-                    <script src="http://fb.me/react-0.12.2.js"></script>
-                    <script src="http://fb.me/JSXTransformer-0.12.2.js"></script>
-                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"></link>
-                    <link rel="stylesheet" href="/crowd-study.css"></link>
-                </head>
-                <body>
-                    <div id="content" className="center-block center-block-no-float">
+            <div>
+                <script src="http://fb.me/JSXTransformer-0.12.2.js"></script>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"></link>
+                <link rel="stylesheet" href="/crowd-study.css"></link>
+                <script src="http://fb.me/react-0.12.2.js"></script>
+                <div id="content" className="center-block center-block-no-float">
                         {this.props.children}
-                    </div>
+                </div>
 
-                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-                </body>
-            </html>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+            </div>
         )
     }
 });
