3ea5916b0b3e285f4279c8b356138b5d8bacaf1d
ncphillips <ncphillips@upei.ca>
2015-03-23 11:42:20 -0300

    Worker bonus stub

--- experiments/external_link/controllers.js
+++ experiments/external_link/controllers.js
@@ -117,6 +117,7 @@ exports.post_form = function (req, res) {
     }
 };
 
+
 /**
  * Generates an alpha-numeric code for validating Workers judgments.
  *
@@ -136,6 +137,16 @@ function generateCode() {
     return result;
 }
 
+
+/**
+ * Gives a Crowdflower worker a bonus.
+ * @param worker_id
+ */
+function crowdflowerBonus(worker_id) {
+
+}
+
+
 /**
  * Receives Crowdflower webhook notices and validates Workers judgments.
  *
@@ -144,24 +155,34 @@ function generateCode() {
  */
 exports.webhook = function (req,res) {
     if (req.body.signal === 'unit_complete') {
+        // Connect to the Worker collection in the database.
         var workers_collection = req.db.collection('workers');
+
+        // For some reason the Crowdflower payload is in a string, so
+        // the body-parser does parse it. No big deal.
         var payload = JSON.parse(req.body.payload);
 
+        // So we want to iterate through every judgment received,
+        // and make sure all of these workers actually did our survey.
         payload.results.judgments.forEach(function(judgment) {
             var worker_id = judgment.worker_id;
-            console.log('Validating work done by: ' + worker_id);
-
             var code = judgment.data.code;
+
+            console.log('Validating work done by: ' + worker_id);
 
             workers_collection.find({id: worker_id}).toArray(function (err, docs) {
                 if (err) {
                     res.status(200).send();
                 }
                 else if (docs.length === 1) {
-                    console.log(docs);
+                    // To do this, we look up the worker with the given id,
+                    // and if the code we stored for him matches the code he
+                    // submitted on Crowdflower, we give a bonus.
                     var worker = docs[0];
                     if (worker.code === code) {
                         console.log('Code matched – Give worker a bonus!');
+                        if (worker.platform === 'crowdflower')
+                            crowdflowerBonus(worker.id);
                     }
                     else {
                         console.log('Code mismatch – No bonus for that worker!')
