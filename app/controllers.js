31dccb3d92c1760019fffe86d1511bb6801a332b
Nolan Phillips <ncphillips.19@gmail.com>
2015-03-25 13:24:16 +0000

    Workers list

--- app/controllers.js
+++ app/controllers.js
@@ -2,3 +2,10 @@ exports.index = function (req, res) {
     res.render('index', {title: 'Crowd Study'});
 };
 
+exports.worker_list = function (req, res) {
+    req.db.collection('workers').find().toArray(function (err, workers) {
+        if (err) return res.status(500).send('Uh oh!');
+        res.render('worker_list', {workers: workers});
+    });
+};
+
