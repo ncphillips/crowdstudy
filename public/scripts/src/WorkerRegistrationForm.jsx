7493bf01cb53f983866fae0021ffdec41fec5b6d
ncphillips <ncphillips@upei.ca>
2015-07-06 13:51:11 -0300

    Worker registration form is fixed

--- public/scripts/src/WorkerRegistrationForm.jsx
+++ public/scripts/src/WorkerRegistrationForm.jsx
@@ -2,27 +2,21 @@
 if (typeof require !== 'undefined') {
   var WorkerActions = require('WorkerActions');
 }
-/**
- * This React component renders a page for capturing a crowd worker's id and platform.
- *
- *
- * @example
- *
- * var MyApp = {
- *   workerRegistered: function (updated_state) {
- *      this.setState(updated_state);
- *   },
- *   render: function () {
- *      return (
- *        <WorkerRegistrationForm callback={this.workerRegistered}>
- *            Here is some additional information pertaining to this experiment.
- *        </WorkerRegistrationForm>
- *      );
- *   }
- * }
- */
+
+
 var WorkerRegistrationForm = React.createClass({
   render: function () {
+    var img_url = '/images';
+    switch (this.state.platform) {
+      case "mturk": img_url += '/mturk_id.png'; break;
+      case "crowdflower": img_url += '/crowdflower_id.png'; break;
+    }
+    var areaClass = "form-group";
+    var errorMessage = null;
+    if (this.state.error) {
+      errorMessage = (<div className="alert alert-danger" role="alert">{this.state.error}</div>);
+      areaClass = "form-group has-error";
+    }
     return (
       <div>
         <div className="col-md-3"></div>
@@ -34,32 +28,46 @@ var WorkerRegistrationForm = React.createClass({
           </div>
           <div className="form-group">
             <label htmlFor="worker-platform">Platform</label>
-            <select id="worker-platform" className="form-control">
+            <select id="worker-platform" ref='platform' className="form-control" onChange={this._onPlatformChange}>
               <option value="crowdflower">CrowdFlower</option>
               <option value="mturk">Amazaon Mechanical Turk</option>
             </select>
           </div>
-          <div className="form-group">
+          <div className={areaClass}>
             <label htmlFor="worker_id">Worker ID</label>
-            <input type="text" id="worker-id" name="worker-id" className="form-control"/>
+            <input type="text" ref='worker_id' id="worker-id" name="worker-id" className="form-control"/>
           </div>
+          {errorMessage}
           <div className="form-group">
             <input type="button" id="worker-id-button"
               className="btn btn-primary form-control"
               value="Submit" onClick={this.submitForm}/>
           </div>
+          <img className="center-block" src={img_url} alt="Uh oh"/>
         </div>
         <div className="col-md-3"></div>
       </div>
     );
   },
-
+  getInitialState: function () {
+    return {
+      platform: "crowdflower",
+      error: ""
+    };
+  },
+  _onPlatformChange: function (e) {
+    this.setState({platform: e.target.value});
+  },
   submitForm: function () {
     var worker = {
       id: document.getElementById('worker-id').value,
       platform: document.getElementById('worker-platform').value
     };
-    WorkerActions.get_or_create(worker);
+    if (worker.id.length > 0) {
+      WorkerActions.get_or_create(worker);
+    } else {
+      this.setState({error: "Please supply your worker ID."});
+    }
   }
 });
 
